6b079e9ba17506153c00b4334dbcb96e7489daa5
soundservice <alan@soundservice.us>
2017-10-10 20:09:13 -0400

    Create js/cell.js

---
+++ js/cell.js
@@ -0,0 +1,16 @@
+area="828-" 
+                        exchange="713-" 
+                        number="7232" 
+                        document.getElementById("cell1").innerHTML =
+                        document.getElementById("cell2").innerHTML =
+                        document.getElementById("cell3").innerHTML =
+                       document.getElementById("cell4").innerHTML =
+                        "<a href='tel:"+area+""+exchange+""+number+"'>"+area+""+exchange+""+number+"</a>";
+             
+
+username="alan" 
+                        at="@" 
+                        domainname="soundservice.us"
+                        document.getElementById("email").innerHTML =
+document.getElementById("email2").innerHTML =
+                        "<a href='mailto:"+username+""+at+""+domainname+"'>"+username+""+at+""+domainname+"</a>";
