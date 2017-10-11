7941248a7daf1b83179c02ad7112488cf2850096
soundservice <alan@soundservice.us>
2017-10-10 20:15:54 -0400

    Create assets/form.js

---
+++ assets/form.js
@@ -0,0 +1,81 @@
+
+var x = document.getElementById("contact_form");
+
+var createform = document.createElement('form'); // Create New Element form
+username="alan" 
+    at="@" 
+  domainname="soundservice.us"
+ 
+	createform.setAttribute("action", "https://formspree.io/"+username+""+at+""+domainname+"");        // Setting action Attribute on form
+	createform.setAttribute("method", "post");  // Setting method Attribute on form
+	x.appendChild(createform);
+
+                       
+
+var heading = document.createElement('h2'); // Heading of form
+	heading.innerHTML = "Contact Form ";  
+	createform.appendChild(heading);
+
+var line = document.createElement('hr');  //giving horizontal row after heading
+	createform.appendChild(line);
+
+var linebreak = document.createElement('br');
+	createform.appendChild(linebreak);
+
+var namelabel = document.createElement('label'); // Create Label for name field
+	namelabel.innerHTML = "Your Name : ";            // Set Field Labels
+	createform.appendChild(namelabel);
+
+var inputelement = document.createElement('input'); // Create input field for name
+	inputelement.setAttribute("type", "text");  
+	inputelement.setAttribute("name", "dname");
+	createform.appendChild(inputelement);
+
+var linebreak = document.createElement('br');
+	createform.appendChild(linebreak);
+
+
+var namelabel = document.createElement('label'); // Create Label for name field
+	namelabel.innerHTML = "Your Phone : ";            // Set Field Labels
+	createform.appendChild(namelabel);
+
+var inputelement = document.createElement('input'); // Create input field for name
+	inputelement.setAttribute("type", "text");  
+	inputelement.setAttribute("name", "dphone");
+	createform.appendChild(inputelement);
+
+var linebreak = document.createElement('br');
+	createform.appendChild(linebreak);
+
+
+
+var emaillabel = document.createElement('label');  //Create Label for email field
+	emaillabel.innerHTML = "Your Email : ";
+	createform.appendChild(emaillabel);
+
+var emailelement = document.createElement('input'); // Create input field for email
+	emailelement.setAttribute("type", "text");
+	emailelement.setAttribute("name", "demail");
+	createform.appendChild(emailelement);
+
+var emailbreak = document.createElement('br');
+	createform.appendChild(emailbreak);
+
+// Append Textarea
+var messagelabel = document.createElement('label'); 
+	messagelabel.innerHTML = "Your Message : please enter date, location or feedback";
+	createform.appendChild(messagelabel);
+
+var texareaelement = document.createElement('textarea'); 
+	texareaelement.setAttribute("name", "dmessage");
+	createform.appendChild(texareaelement);
+
+var messagebreak = document.createElement('br');
+	createform.appendChild(messagebreak);
+
+// Append Submit Button
+var submitelement = document.createElement('input'); 
+	submitelement.setAttribute("type", "submit");
+	submitelement.setAttribute("name", "dsubmit");
+	submitelement.setAttribute("value", "Submit");
+	createform.appendChild(submitelement);
